9dc31c5c397d5d3cc7261f2ef70e9c9f87494966
reztirps <spritzer@gmail.com>
2023-09-21 18:38:38 -0300

    Update geometric.ts

--- random-graph-generators/geometric.ts
+++ random-graph-generators/geometric.ts
@@ -1,5 +1,5 @@
-import { Graph } from "../../models/graph";
-import { RandomGraphGenerator } from "../../models/random-graph-generator";
+import { Graph } from "../models/graph";
+import { RandomGraphGenerator } from "../models/random-graph-generator";
 
 export class GeometricRandomGraph implements RandomGraphGenerator {
   graph: Graph = { nodes: [], edges: [] };
